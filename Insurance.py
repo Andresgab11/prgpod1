87845cfa8fbfa9014b7a4fb830e2e674b9c6a99c
Andresgab11 <A01734475@itesm.mx>
2020-10-28 23:25:16 +0000

    m

---
+++ Insurance.py
@@ -0,0 +1,6 @@
+import csv
+archivo = open("insurance.csv")
+
+for linea in csv.DictReader(archivo):
+    print(linea["age"])
+    
---
+++ estadistica.py
@@ -0,0 +1,5 @@
+import pandas as pd
+import numpy as np
+df = pd.read_csv("ulabox_orders_with_categories_partials_2017.csv")
+tamaño = df.shape()
+print(tamaño)
