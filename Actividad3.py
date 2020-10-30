0bf127bcc1db49c4b22909f07e41d4a921fec98c
Andresgab11 <A01734475@itesm.mx>
2020-10-30 03:44:24 +0000

    g

--- Actividad3.py
+++ Actividad3.py
@@ -9,13 +9,12 @@ df.describe()
 
 # %%
 import seaborn as sns
-sn =  sns.read_csv("ulabox_orders_with_categories_partials_2017.csv")
 sns.set_theme(style="white")
 
 # %%
 sns.displot(x="customer", data=df)
 
 # %%
-sn.boxplot(by ='discount',grid='True',column =['weekday'], color='red')
+df.boxplot(by ='weekday',grid='True',column =['discount'], color='red')
 
 # %%
